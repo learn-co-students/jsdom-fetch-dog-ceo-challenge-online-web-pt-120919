e2453ff49c20eeddfc53ed82b6b029aa419c118d
cep104 <candicelizabethpeters@gmail.com>
2020-06-30 21:55:00 -0500

    picutres figured out

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImage(){
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => renderImages(json))
+}
+
+function renderImages(images) {
+    const imagesMessage = images.message
+    
+    for (const image of imagesMessage){
+        appendImage(image)
+    }
+  }
+
+  function appendImage(image){
+    const dogPic = document.getElementById('dog-image-container')
+    const imageTag = document.createElement('img')
+    
+    imageTag.src = image
+    dogPic.appendChild(imageTag)
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchImage()
+  })
