2d524b9fd3c8a4d252bef51527b12d21d81c115c
SMakaiTakori <makait87@icloud.com>
2020-07-06 16:43:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -20,20 +20,20 @@ document.addEventListener("DOMContentLoaded", function(e){
     fetch("https://dog.ceo/api/breeds/list/all") 
     .then(response => response.json())
     .then(data => {
-        console.log("hey!")
+      
         const breedObject = data.message
         const breedList = document.getElementById("dog-breeds")
         breedList.innerHTML = ""
         for(const breed in breedObject){
             // console.log(breed)
             const option = document.querySelector("select").value
-            // const list = document.createElement("ul")
+            
             const newList = document.createElement("li")
+            
             function colorChange() {
-                // listItem = document.querySelector("li")
-                // listItem.style.color="red"
                 event.target.style.color="red"
             }
+
             if(breed[0] === option){
                 newList.innerText = breed
                 newList.addEventListener("click", colorChange)
